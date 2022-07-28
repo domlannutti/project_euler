6339f6e7ad7d655f171765956449282d78a7f13b
Dominic Lannutti <34784127+domlannutti@users.noreply.github.com>
2022-07-27 22:49:23 -0500

    Update 4_Largest_Palindrome_Product.py
    Added function annotation

--- 4_Largest_Palindrome_Product.py
+++ 4_Largest_Palindrome_Product.py
@@ -3,12 +3,12 @@
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
 ## Solution
-def is_palindrome(num):
+def is_palindrome(num: int) -> bool:
     if str(num) == str(num)[::-1]:
         return True
     return False
 
-def main():
+def main() -> int:
     pal = []
     for i in range(100, 1000):
         for j in range(100, i):
