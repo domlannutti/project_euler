f587497e9d8da6dbe726c47dcdc646006340e016
Dominic Lannutti <34784127+domlannutti@users.noreply.github.com>
2022-07-29 11:51:26 -0500

    Add files via upload
    Initial commit

---
+++ 9_Special_Pythagorean_Triplet.py
@@ -0,0 +1,21 @@
+## Problem statement
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+## Solution
+def main(target):
+    for m in range(3, target, 2):
+        for n in range(1, m, 2):
+            a = m * n
+            b = (m**2 - n**2) // 2
+            c = (m**2 + n**2) // 2
+            if a+b+c == target:
+                return a*b*c
+    return 0
+
+
+if __name__ == "__main__":
+    print(main(1000))
